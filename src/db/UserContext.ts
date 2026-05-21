88a5ccf807f2446879a028fd1c03662c5fa10823
MrSamuel0 <samuca.manhani@gmail.com>
2026-05-21 09:40:48 -0300

    Refactor: Changes to project structure

--- src/db/UserContext.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export default class UserContext {
-    async connect(connString: string) {
-        await mongoose.connect(connString)
-    }
-}
--- src/repository/IUserRepository.ts
+++
@@ -1,8 +0,0 @@
-import { IUser } from "../models/UserModel"
-
-export default interface IUserRepository{
-    createUser: (user: IUser) => Promise<IUser>;
-    updateUser: (id: number, user: IUser) => Promise<IUser | null>;
-    getUser: (id: number) => Promise<IUser | null>;
-    deleteUser: (id: number) => Promise<void>;
-}
